2158e05b1caf144475734f737ccee305c4976e5c
Federico <federico.caiello@gmail.com>
2020-01-15 14:05:39 +0100

    pulizia codice

--- js/script.js
+++ js/script.js
@@ -1,25 +1,19 @@
 // Campo Minato
-// Il computer deve generare 16 numeri casuali da 1 a 100.
-// In seguito deve chiedere all’utente di inserire per 84 volte un numero da 1 a 100, se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti continua chiedendo all’utente un altro numero.
-// La partita termina quando il giocatore inserisce un numero “vietato”, ovvero presente nella lista di numeri random, o raggiunge il numero massimo possibile di tentativi consentiti.
-// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
-
-
-
-var listaNumeriPc = [29,];
+var listaNumeriPc = [];
 var numeroUtente;
 var esito;
 var punteggio = 0;
-
-for (i=0; i<16; i++) {
+// Il computer deve generare 16 numeri casuali da 1 a 100.
+for (var i=0; i<16; i++) {
   listaNumeriPc.push(getRandomNumer(1, 100));
 }
 console.log(listaNumeriPc);
-
-for (i=0; i<84; i++) {
+// In seguito deve chiedere all’utente di inserire per 84 volte un numero da 1 a 100, se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti continua chiedendo all’utente un altro numero.
+for (var i=0; i<84; i++) {
   numeroUtente = parseInt(prompt('inserisci un numero da 1 a 100'));
   esito = comparazione(listaNumeriPc, numeroUtente);
+  // La partita termina quando il giocatore inserisce un numero “vietato”, ovvero presente nella lista di numeri random, o raggiunge il numero massimo possibile di tentativi consentiti.
   if (esito == true) {
     i = 84;
     alert('hai perso');
@@ -27,17 +21,17 @@ for (i=0; i<84; i++) {
     alert('che culo hai vinto');
     i = 84;
   }
+  // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
   else {
     punteggio++;
   }
-
 }
 alert('il tuo punteggio e\'' + punteggio);
 console.log(punteggio);
-
-
+                                                    
+// FUNZIONE
 function comparazione(lista, numero) {
-  for (i=0; i < lista.length; i++) {
+  for (var i=0; i < lista.length; i++) {
     if (numero == lista[i]) {
       return true;
     }
